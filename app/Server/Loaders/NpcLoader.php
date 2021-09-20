c72ef14124b4d3a689307af923f2391e7450d966
Homeleonn <wirus@ukr.net>
2021-09-20 23:44:45 +0300

    add login to npc

--- app/Server/Loaders/NpcLoader.php
+++ app/Server/Loaders/NpcLoader.php
@@ -13,6 +13,7 @@ class NpcLoader
 		$npc = DB::getAll('SELECT id, name, level, curhp, maxhp, power, critical, evasion, stamina, aggr, is_undead, image FROM npc');
 		$npc = Common::itemsOnKeys($npc, ['id'], function($npc) {
 			[$npc->min_damage, $npc->max_damage] = User::calculateDamage($npc->power);
+			$npc->login = $npc->name;
 		});
 
 		return $npc;
